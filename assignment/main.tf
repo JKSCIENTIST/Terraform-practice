0f72c95c7cdb4dc601b5571ecb7bc24478f48b78
kaushikjk <kaushikjk18@gmail.com>
2024-01-30 20:51:19 +0530

    Updated the code with versioning and acl options

--- assignment/main.tf
+++ assignment/main.tf
@@ -6,10 +6,10 @@ resource "aws_s3_bucket" "Terra-bucket"{
 
   bucket = var.bucket_name
   tags = {Name = var.bucket_tag }
-
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
+    count = var.versioning_enabled ? 1:0
     bucket = aws_s3_bucket.Terra-bucket.id
     versioning_configuration {
         status = "Enabled"
@@ -17,19 +17,20 @@ resource "aws_s3_bucket_versioning" "versioning" {
 }
 
 resource "aws_s3_bucket_acl" "name" {
+  acl = var.acl_access ? "private":"public-read"
   bucket = aws_s3_bucket.Terra-bucket.id
   depends_on = [ aws_s3_bucket_ownership_controls.ownership , aws_s3_bucket_public_access_block.access ]
-  acl = "public-read"
 }
 
 resource "aws_s3_bucket_public_access_block" "access" {
   bucket = aws_s3_bucket.Terra-bucket.id
 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
+  block_public_acls = var.acl_access ? true : false
+  block_public_policy = var.acl_access ? true : false
+  ignore_public_acls = var.acl_access ? true : false
+  restrict_public_buckets = var.acl_access ? true : false
 }
+
 
 resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.Terra-bucket.id
--- assignment/variables.tf
+++ assignment/variables.tf
@@ -7,3 +7,15 @@ variable "bucket_tag" {
   description = "Tag for the bucket!"
   type = string
 }
+
+variable "versioning_enabled" {
+  description = "true to enable it and false to disable it"
+  type = bool
+  # default = false
+}
+
+variable "acl_access" {
+  description = "true for private ACL, false for public ACL"
+  type = bool
+  # default = false
+}
